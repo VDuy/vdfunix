9da15d81ec3632372b604cf19ed4a3fb4c3af6b9
Vũ Duy <ducduy23899@gmail.com>
2022-03-25 00:47:24 +0700

    6

--- prf192x_2.1-a_vn_dn/lab6.js
+++ prf192x_2.1-a_vn_dn/lab6.js
@@ -5,9 +5,9 @@ let arr2 = [12, 5, -5, 0, 4];
 const printForecast = function (arr) {
     let str = '';
     for (let i = 0; i < arr.length; i++) {
-        str = str + `${arr[i]}ºC in ${i+1} days ... `;
+        str = str + `${arr[i]}ºC in ${i + 1} days ... `;
     }
-    console.log('...'+ str);;
+    console.log('...' + str);;
 };
 printForecast(arr1);
 printForecast(arr2);
